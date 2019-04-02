acca56a86e02e4a48863e2f9fe9fc97580f115a8
Tommiee <tomdepom1999@gmail.com>
2019-04-02 16:17:45 +0200

    Unity VR input using SteamVR

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	private int TotalObjects;
+
+	public void Spawn(GameObject _obj,int amount){
+		for(int i = 0; i < (int)amount; i++){
+			for(int j = 0; j < (int)amount;j++){
+				Instantiate(_obj,new Vector3(0+(j/16), 2, 0+(i/16)),Quaternion.identity);
+				TotalObjects++;
+			}
+		}
+	}
+
+	public void PrintTotal(){
+		print("Totaal aantal objects: " + TotalObjects); 
+	}
+}
---
+++ Assets/Scripts/ViveInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour
+{		
+	public SteamVR_ActionSet _ActionSet;
+	private Spawner _spawner;
+	[SerializeField]private GameObject _obj;
+
+	private void Start(){
+		_ActionSet.Activate(SteamVR_Input_Sources.Any,0,true);
+		_spawner = gameObject.GetComponent<Spawner>();
+	}
+
+	void Update(){
+		if(SteamVR_Actions._default.Teleport.GetStateDown(SteamVR_Input_Sources.Any)){
+			_spawner.Spawn(_obj,5);
+		}
+
+		if(SteamVR_Actions._default.GrabGrip.GetStateDown(SteamVR_Input_Sources.Any)){
+			_spawner.Spawn(_obj,10);
+		}
+
+		if(SteamVR_Actions._default.GrabPinch.GetStateDown(SteamVR_Input_Sources.Any)){
+			_spawner.PrintTotal();
+		}
+	}
+}
