2a34030e06771fef589043afdd2f1ea0dfa24f48
Gijs Schouten <21969@ma-web.nl>
2019-04-11 14:42:42 +0200

    video script

---
+++ Assets/Scripts/video/Video.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video : MonoBehaviour {
+	private VideoPlayer videoPlayer;
+	
+	[SerializeField]
+	private VideoClip _video;
+	private bool _rewind = false;
+
+	void Start() {
+		StartCoroutine(PlayVideo());
+	}
+
+	IEnumerator PlayVideo() {
+		GameObject videoObject = GameObject.Find("VideoObject");
+		videoPlayer = videoObject.AddComponent<VideoPlayer>();
+		videoPlayer.playOnAwake = false;
+		videoPlayer.clip = _video;
+		videoPlayer.Prepare();
+		videoPlayer.SetDirectAudioMute(0, true);
+
+		
+
+		while (!videoPlayer.isPrepared) {
+			Debug.Log("Preparing Video");
+			yield return null;
+		}
+
+		videoPlayer.Play();
+
+		while (videoPlayer.isPlaying) {
+			Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+			yield return null;
+		}
+	}
+
+	public void ChangeTime(int seconds) {
+		videoPlayer.time = seconds;
+	}
+
+	public void Pause() {
+		videoPlayer.Pause();
+	}
+
+	public void Play() {
+		videoPlayer.Play();
+	}
+
+	public void Rewind() {
+		if (_rewind) {
+			videoPlayer.Play();
+			_rewind = false;
+		}
+
+		_rewind = true;
+		videoPlayer.Pause();
+
+	}
+
+	public void PlayNormal() {
+		videoPlayer.playbackSpeed = 1;
+	}
+
+	public void FastForward(float amount) {
+		videoPlayer.playbackSpeed = amount;
+	}
+
+	private void Update() {
+		if (_rewind) {
+			videoPlayer.frame--;
+		}
+
+		if (videoPlayer.frame <= 0) {
+			_rewind = false;
+		}
+	}
+}
