a5ba78db545792842a009974bf7d2da4a7713277
lucasmuileboom <muileboomlucas@gmail.com>
2019-04-11 10:55:55 +0200

    added video controle interface

---
+++ Assets/Scripts/UI/VideoControleInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VideoControleInput : MonoBehaviour
+{
+    public SteamVR_ActionSet _ActionSet;
+    public SteamVR_Input_Sources Hand;
+
+    [SerializeField] private bool touchPadButtonMode;
+
+    void Start()
+    {
+        _ActionSet.Activate(Hand, 0, true);
+    }
+    void Update()
+    {
+        if (touchPadButtonMode)
+        {
+            TouchpadButtonsControler();
+        }
+    }
+
+    private void TouchpadButtonsControler()
+    {
+        Vector2 trackpad = SteamVR_Actions._default.trackpad.GetAxis(Hand);
+        float _touchPadPositionX = trackpad.x;
+        //float _touchPadPositionY = trackpad.y;
+
+        if (SteamVR_Actions._default.Teleport.GetStateDown(Hand))
+        {
+            //print(_touchPadPositionX + "<>" + _touchPadPositionY + ">>" + trackpad);
+            if (_touchPadPositionX < -0.5f)
+            {
+                print("rewind");
+            }
+            else if (_touchPadPositionX > -0.5f && _touchPadPositionX < 0.5f)
+            {
+                print("pause");
+            }
+            else if (_touchPadPositionX > 0.5f)
+            {
+                print("fastforward");
+            }
+        }
+    }
+}
