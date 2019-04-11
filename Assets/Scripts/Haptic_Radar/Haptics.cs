a0124b6ca09b0f9734b3845ba872741d0f2af75d
Tommiee <tomdepom1999@gmail.com>
2019-04-11 11:57:52 +0200

    Updated scene, Updated Haptics
    Scene:
    - Moved some objects around
    - fixed assigned controller
    Haptics:
    - Made input run on event listeners instead of update
    - Renamed some variables
    - Removed todo's

--- Assets/Scripts/Haptic_Radar/Haptics.cs
+++ Assets/Scripts/Haptic_Radar/Haptics.cs
@@ -6,8 +6,8 @@ using Valve.VR;
 public class Haptics : MonoBehaviour
 {
 
-	public SteamVR_Action_Vibration hapticAction;
-	public SteamVR_Action_Boolean grabPinchAction;
+    public SteamVR_Action_Vibration _hapticOutput;
+	public SteamVR_Action_Boolean _buttonAction;
 
     private GameObject _closest;
     private float _distToClosest;
@@ -15,46 +15,40 @@ public class Haptics : MonoBehaviour
     [SerializeField]
     private string _tag;
     [SerializeField]
-    private GameObject _player;
+    private GameObject _controller;
 
     private FindObject _objectFinder;
 
     private void Start()
     {
         _objectFinder = gameObject.GetComponent<FindObject>();
+        _buttonAction.AddOnChangeListener(PinchTrigger, SteamVR_Input_Sources.LeftHand);
     }
 
-
-    //todo: make this run on event listeners instead of in update
-
-    void Update()
+    private void PinchTrigger(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool isDown)
     {
-		if(grabPinchAction.GetStateDown(SteamVR_Input_Sources.LeftHand)){
-			Pulse(2, 0.2f, 200f, 0.1f, SteamVR_Input_Sources.LeftHand);
-            _closest = _objectFinder.FindClosestTarget(_player, _tag);
-            StartCoroutine(Radar(_closest, _player));
-        }
-        if (grabPinchAction.GetStateUp(SteamVR_Input_Sources.LeftHand))
+        if (isDown)
         {
-            Pulse(2, 0.2f, 75f, 0.1f, SteamVR_Input_Sources.LeftHand);
+            _closest = _objectFinder.FindClosestTarget(_controller, _tag);
+            StartCoroutine(Radar(_closest, _controller, SteamVR_Input_Sources.LeftHand));
+        } else
+        {
             StopAllCoroutines();
         }
     }
 
 	private void Pulse(int pulseAmount, float duration, float frequency, float amplitude, SteamVR_Input_Sources source){
 		for(int i = 0; i < pulseAmount; i++){
-			hapticAction.Execute(0,duration,frequency,amplitude,source);
+			_hapticOutput.Execute(0,duration,frequency,amplitude,source);
 		}
 	}
 
-    IEnumerator Radar(GameObject _obj, GameObject _player, bool _stop = false)
+    IEnumerator Radar(GameObject _obj, GameObject _player, SteamVR_Input_Sources source)
     {
-        while (!_stop)
+        while (!false)
         {
             _distToClosest = Vector3.Distance(_player.transform.position, _obj.transform.position);
-            print("Dist to Closest: " + _distToClosest);
-            //todo: make amplitude inversely quadratic within clamp
-            Pulse(1, 0.2f, Mathf.Round(320f/_distToClosest), Mathf.Clamp(1f - (_distToClosest / 10),0f,1f), SteamVR_Input_Sources.LeftHand);
+            Pulse(1, 0.2f, Mathf.Round(300f/_distToClosest), Mathf.Clamp(1f - (_distToClosest / 10),0f,1f), source);
             yield return new WaitForSeconds(0.75f);
         }
     }
