8b167d091e20c576baeabe5a69dd3169d936e610
Tommiee <tomdepom1999@gmail.com>
2019-04-12 13:14:32 +0200

    Fixed pulse coroutine, minor value adjustments

--- Assets/Scripts/Haptic_Radar/Haptics.cs
+++ Assets/Scripts/Haptic_Radar/Haptics.cs
@@ -40,8 +40,7 @@ public class Haptics : MonoBehaviour
 	IEnumerator Pulse(int pulseAmount, float duration, float frequency, float amplitude, SteamVR_Input_Sources source){
 		for(int i = 0; i < pulseAmount; i++){
 			_hapticOutput.Execute(0,duration,frequency,amplitude,source);
-            print("pulse");
-            yield return new WaitForSeconds(duration);
+            yield return new WaitForSeconds(duration+0.05f);
 		}
         yield break;
 	}
@@ -53,11 +52,11 @@ public class Haptics : MonoBehaviour
             _distToClosest = Vector3.Distance(_player.transform.position, _obj.transform.position);
             if(_distToClosest >= 0.1)
             {
-                Pulse(1, 0.2f, Mathf.Clamp(Mathf.Round(320f / _distToClosest), 5, 300), Mathf.Clamp(1f - (_distToClosest / 10), 0f, 1f), source);
+                StartCoroutine(Pulse(1, 0.2f, Mathf.Clamp(Mathf.Round(320f / _distToClosest), 1, 320), Mathf.Clamp(1f - (_distToClosest / 10), 0f, 1f), source));
                 yield return new WaitForSeconds(0.75f);
             } else
             {
-                Pulse(2, 0.75f, 100f, 1f, source);
+                StartCoroutine(Pulse(2, 0.5f, 50f, 1f, source));
                 yield break;
             }
         }
